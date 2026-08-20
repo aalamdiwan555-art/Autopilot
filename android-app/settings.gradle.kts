3437e79f7631ce0c2298d1bdd835d49a1e0dc044
aalamdiwan555-art <aalamdiwan555@gmail.com>
2026-08-20 16:44:43 +0530

    Update repositories in settings.gradle.kts
    Added StartApp Maven repository to dependency resolution.

--- android-app/settings.gradle.kts
+++ android-app/settings.gradle.kts
@@ -1,7 +1,17 @@
-pluginManagement { repositories { google(); mavenCentral(); gradlePluginPortal() } }
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories { google(); mavenCentral() }
+    repositories {
+        google()
+        mavenCentral()
+        maven { url 'https://maven.startapp.com/artifactory/libs-release' } // ye line add ki
+    }
 }
 rootProject.name = "Autopilot"
 include(":app")
